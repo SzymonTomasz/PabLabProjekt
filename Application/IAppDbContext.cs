fceca7b545224baed045e8ae7efee2e474ae8af5
Szymon Górski <gorskiszymon@gmail.com>
2024-06-06 19:35:59 +0200

    added NuGet packages and dependencies

---
+++ Application/IAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Interfaces
+{
+    public interface IAppDbContext
+    {
+        public DbSet<Zoo> Zoos { get; set; }
+
+        public DbSet<Animal> Animals { get; set; }
+
+        public DbSet<Location> Locations { get; set; }
+
+        void SaveChanges();
+    }
+}
---
+++ Infrastructure/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Infrastructure
+{
+
+    public class AppDbContext : IdentityDbContext, IAppDbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Zoo> Zoos { get; set; }
+
+        public DbSet<Animal> Animals { get; set; }
+
+        public DbSet<Location> Locations { get; set; }
+
+        void IAppDbContext.SaveChanges()
+        {
+            this.SaveChanges();
+        }
+    }
+}
